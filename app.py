bd87380de47a1d14a453fd17c729776dd7333bd3
Venkatsaikumar42 <rbvsaikumar42@gmail.com>
2024-03-01 19:59:56 -0500

    Updated Code and changed the output format for items.json

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import json
-filename='example_orders.json'
+import argparse
+parser = argparse.ArgumentParser(description='Provide the Json File name')
+parser.add_argument('filename', type=str, help='Provide the filename containing json objects')
+args = parser.parse_args()
+filename=args.filename
 with open(filename,'r') as f:
     orderdata=json.load(f)     
-customers=[{order["phone"]:order["name"]} for order in orderdata]
 items_info = {}
+customerdata={}
 for order in orderdata:
+    customerdata[order['phone']]=order['name']
     for item in order["items"]:
         item_name = item["name"]
         items_info.setdefault(item_name, {"price": item["price"], "orders": 0})
         items_info[item_name]["orders"] +=1
 
-print(customers)
 print("-----------\n")
 print(items_info)
 
 customer_output_file='customers.json'
 items_output_file='items.json'
 with open(customer_output_file, 'w') as file:
-    json.dump(customers,file,indent=4)
+    json.dump(customerdata,file,indent=4)
 with open(items_output_file, 'w') as file:
     json.dump(items_info,file,indent=4)
